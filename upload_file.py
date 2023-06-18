098d99dd423a892a57f8061524f52b8f665b96fa
SheepMan <pepito.mussiloni@gmail.com>
2023-06-18 21:13:28 +0300

    changed the way code evaluates successful upload

--- upload_file.py
+++ upload_file.py
@@ -3,7 +3,7 @@ import logging
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
-
+from selenium.common.exceptions import NoSuchElementException
 
 def run_upload():
     file_path = 'W:\\the_internet_project\\downloads\\robot.txt'
@@ -17,11 +17,15 @@ def run_upload():
     cofirm_upload_button = browser.find_element(By.ID, 'file-submit')
     cofirm_upload_button.click()
     time.sleep(2)
-    if (browser.page_source.find("File Uploaded!")):
+    try:
+        is_visible = browser.find_element(By.ID, "uploaded-files").is_displayed()
+    except NoSuchElementException:
+        is_visible = False
+    if (is_visible):
            print("File uploaded!")
     else:
         print("File is not uploaded!")
-    
+
     
 
 run_upload()
