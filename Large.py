edbb06277b8a686455d6d1673ef5c5642fd8d6ba
SheepMan <pepito.mussiloni@gmail.com>
2023-06-28 15:27:39 +0300

    changed the code to run 1000 times faster(not a joke), also changed the output to one line

--- Large.py
+++ Large.py
@@ -5,17 +5,20 @@ import time
 def verify_elements_presence():
     browser = webdriver.Chrome()
     browser.get("https://the-internet.herokuapp.com/large")
+    counts = []
     for row_num in range(1, 51):
         row_class = f"row-{row_num}"
         row = browser.find_element(By.CLASS_NAME, row_class)
+        one_l = 0
         for col_num in range(1, 51):
             col_class = f"column-{col_num}"
             column = row.find_element(By.CLASS_NAME, col_class)
-            for f_num in range(1,51):
-                for s_num in range(1, 51):
-                    column_text = f"{f_num}.{s_num}"
-                    if column.text == column_text:
-                        print("all objects present")
-                    else: print("not all objects present")
 
+            if column.is_displayed():
+                one_l += 1 
+        counts.append(one_l)
+    all_table_elements = sum(counts)
+    if all_table_elements == 2500:
+        print("all elements are present")
+    else: print(f"only {all_table_elements} of 2500 elements are present")
 verify_elements_presence()
